70510a4496cc06edcf3d28d055ed9cc9e5a652d1
voidJeff <qy120838@126.com>
2015-09-17 15:17:07 -0400

    Updated from Brython Server: 9/17/2015 3:17:06 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jeff
+Credit: Wikipedia
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,12 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+width = int(input("Width of multiplication table: "))
+height = int(input("Height of multiplication table: "))
+n = 1
+while height > 0:
+    for i in range(1,width+1):
+        print("{0:>3}".format(i * n), end = " ")
+    height -= 1
+    n += 1
+    print("")
